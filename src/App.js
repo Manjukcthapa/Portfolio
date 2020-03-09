cab6f3210276aad0ddb4c112bdbc54a53cb883ca
Manju KC <kcmanjuthapa@gmail.com>
2020-03-09 17:53:25 -0400

    added eucation component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import Home from './Component/homepage';
 import Projects from './Component/project'
+import Education from './Component/Education'
 import "./App.css";
 
 
@@ -17,6 +18,7 @@ class App extends Component {
             <li><Link to={'/'} className="nav-link"><span>Home</span></Link></li>
             <li><Link to={'/skills'} className="nav-link"><span>Skills</span></Link></li>
             <li><Link to={'/Project'} className="nav-link"><span>Project</span></Link></li>
+            <li><Link to={'/Education'} className="nav-link"><span>Education</span></Link></li>
           </ul>
           </nav>
           <hr />
@@ -24,6 +26,7 @@ class App extends Component {
               <Route exact path='/' component={Home} />
               <Route path='/skills' component={Skills} />
               <Route path='/Project' component={Projects}/>
+              <Route path='/Education' component={Education}/>
           </Switch>
         </div>
       </Router>
---
+++ src/Component/Education.js
@@ -0,0 +1,33 @@
+import React from "react";
+import "./education.css";
+
+class Education extends React.Component {
+  render() {
+    return (
+      <div className="education">
+        <h3>Lambda School of Computer Science</h3>
+        <p>
+          Lambda School is a 40 week Full-Time, All-Immersive online bootcamp.
+          They break down by the week on what you learn, Units are then broken
+          into 4 weeks. At the end of every 3 weeks, the students do a school
+          wide build-week where students all over the school come together and
+          build a project using the skills they learned over the last 4 weeks,
+          repeating the process 4 times making the main portion 16 weeks long!
+        </p>
+        <p>
+          After that, students proceed to Lambda Labs where they work on a
+          Full-Scale, Real-World Applications and finish them within 8 weeks.
+          The beginning is dedicated to planning, then into building after week
+          3.
+        </p>
+        <p>
+          Finally, students end in an 8 week Computer Science program. Here they
+          cover: Intro to Python and OOP, Algorithms, Data Structures, Hash
+          Tables and Blockchains, Graphs and Computer Architecture.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default Education;

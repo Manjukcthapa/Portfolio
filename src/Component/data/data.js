9bd6f634d19c0aa2c3d35e64abcfc9a1f830437f
Manju KC <kcmanjuthapa@gmail.com>
2020-03-10 23:07:34 -0400

    added title and technology in component

--- src/Component/data/data.js
+++ src/Component/data/data.js
@@ -7,16 +7,19 @@ import image3 from '../../images/nci.png';
     {
         "id":1,
         "img":image,
-        "contain":"This is the first step of coding journey.Learning HTML, CSS and version control. It was the first project working with the group and bulid the single page application using HTML5 and CSS. It was really fun and excited.",
+        "title": "Pocaso Web App",
+        "contain":"Picaso is an web app, where you can upload your pic and it coverts to Picaso painting.",
+        "technology": "ReactJS, HTML, CSS",
         "githublink":"//www.github.com/buildweekpicasso",
         "livelink":"https://picasso-manju.now.sh/"
-
     },
 
     {
         "id":2,
         "img":image1,
-        "contain":"Mympy Dreams is a marketplace where individuals can create a profile and post their project and needs and find funding from the Mymphy Dreams community.In this project we have used  React, Redux, Styled Component, Nodejs, Postgress,JSON, AuthO, Web token",
+        "title":"Mympy Dreamers",
+        "contain":"Mympy Dreams is a marketplace where individuals can create a profile and post their project and needs and find funding from the Mymphy Dreams community.",
+        "technology":"React, Redux, Postgress, NodeJs",
         "githublink":"https://github.com/mympy-dreamers",
         "livelink":"https://mympydreamers.netlify.com/"
 
@@ -24,7 +27,9 @@ import image3 from '../../images/nci.png';
     {
         "id":3,
         "img":image2,
-        "contain":"Single Project application for tourist lovers. A place for guide and tourist to meet and share their experience.work as a frontend developer on a team with six developer. Used React, React router, Axios, CSS for styling." ,
+        "title":"Wanderlust",
+        "contain":"Single Project application for tourist lovers. A place for guide and tourist to meet and share their experience." ,
+        "technology":"ReactJS, CSS",
         "githublink":"https://github.com/wanderlust-bw",
         "livelink":"https://wonderlust-manju.herokuapp.com/SignUp"
 
@@ -32,7 +37,9 @@ import image3 from '../../images/nci.png';
     {
         "id":4,
         "img":image3,
-        "contain":" Single page application for international child nutrition screening.In the world there are lot of county dont have child screening record. The purpose of creating this application was recording the child nutrition screeing for forever. As a backend I have used NodeJS, sQLite3, bcrypt",
+        "title":"Intl Child Nutrition",
+        "contain":" Single page application for international child nutrition screening.In the world there are lot of county dont have child screening record. The purpose of creating this application was recording the child nutrition screeing for forever.",
+        "technology":"Nodejs, Sqlite3",
         "githublink":"https://github.com/intlChildNutrition",
         "livelink":"https://icn-tracking.netlify.com/dashboard"
 
--- src/Component/projectlist.js
+++ src/Component/projectlist.js
@@ -1,29 +1,35 @@
-import React, { Component } from "react"
-import data from "../Component/data/data.js"
+import React, { Component } from "react";
+import data from "../Component/data/data.js";
 import { Link } from "react-router-dom";
 
 class ProjectList extends Component {
-    render(){
-        return(
-           <div>
-               <h1>My Projects</h1>
-               {data.map((post, id) => {
-                   return <div>
-                       <img src={post.img}></img>
-                       <h1>{post.contain}</h1>
-                       <div>
-                       <a href={post.githublink}><button>Github</button></a>
-                       </div>
-                       <div>
-                       <a href={post.livelink}><button>LiveLink</button></a>
-                       </div>
-                   </div>
-               })
-
-               }
-           </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <h1>My Projects</h1>
+        {data.map((post, id) => {
+          return (
+            <div>
+              <img src={post.img}></img>
+              <h1>{post.title}</h1>
+              <h1>{post.contain}</h1>
+              <h1>{post.technology}</h1>
+              <div>
+                <a href={post.githublink}>
+                  <button>Github</button>
+                </a>
+              </div>
+              <div>
+                <a href={post.livelink}>
+                  <button>LiveLink</button>
+                </a>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
 }
 
-export default ProjectList
+export default ProjectList;

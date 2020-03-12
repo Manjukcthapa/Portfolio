027a0135899e75988473db3ac1c07b36c116f433
Manju KC <kcmanjuthapa@gmail.com>
2020-03-11 20:26:10 -0400

    changed home link to about me

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ class App extends Component {
         <div className="App">
           <nav className="nav">
           <ul>
-            <li><Link to={'/'} className="nav-link"><span>Home</span></Link></li>
+            <li><Link to={'/'} className="nav-link"><span>About Me</span></Link></li>
             <li><Link to={'/skills'} className="nav-link"><span>Skills</span></Link></li>
             <li><Link to={'/Project'} className="nav-link"><span>Project</span></Link></li>
             <li><Link to={'/Education'} className="nav-link"><span>Education</span></Link></li>

89da6c21004fdb65b1e1e3923191d6314f12c7c9
Manju KC <kcmanjuthapa@gmail.com>
2020-07-14 12:50:36 -0400

    changed the style of contactcomponent

--- src/Component/Contact .js
+++ src/Component/Contact .js
@@ -1,26 +1,48 @@
-import React from "react"
+import React from "react";
 import "./contact.css";
 
 class Contact extends React.Component {
-render(){
-    return(
+  render() {
+    return (
+      <div className="information">
         <div className="contact">
-           <div>
-               <h2>MY Phone</h2>
-               <p>8162660106</p>
-           </div> 
-           <div>
-               <h2>Email Address</h2>
-               <p>kcmanjuthapa@gmail.com</p>
-           </div> 
-           <div>
-           <h2>Location</h2>
-           <p>West Paces Ferry Rd, GA</p>
-           </div>
-        </div>
-    )
-}
+          <h3 className="header3">MY Phone</h3>
+          <div className="contacticon">
+            <div>
+              <i class="fa fa-phone-square fa-2x" aria-hidden="true"></i>
+            </div>
+            <div>
+              <p className="phonenumber">8162660106</p>
+            </div>
+          </div>
 
+          <div>
+            <h3 className="header3">Email Address</h3>
+            <div className="contacticon">
+              <div>
+                <i class="fa fa-envelope fa-2x" aria-hidden="true"></i>
+              </div>
+              <div>
+                <a className="phonenumber" href="">kcmanjuthapa@gmail.com</a>
+              </div>
+            </div>
+          </div>
+
+          <div>
+            <h3 className="header3">Location</h3>
+            <div className="contacticon">
+              <div>
+                <i class="fa fa-map-marker fa-2x" aria-hidden="true"></i>
+              </div>
+              <div>
+                <p className="phonenumber">West Paces Ferry Rd, GA</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Contact;

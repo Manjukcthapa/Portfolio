1178230997981b3018ef4c5296ecf99f28ee68cf
Manju KC <kcmanjuthapa@gmail.com>
2020-07-16 14:00:00 -0400

    updated resume link

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Home from './Component/Homepage';
 import Project from './Component/Projectlist'
 import Education from './Component/Education'
 import Contact from './Component/Contact'
-import Resume from './../public/ManjuFinalResume.pdf'
+import Resume from './../public/ManjuResume.pdf'
 import "./App.css";
 
 class App extends Component {

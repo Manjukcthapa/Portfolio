ec5bb06ca9219e0ed2f8de4c36003a3ba3d67e44
Manju KC <kcmanjuthapa@gmail.com>
2020-10-18 11:07:16 -0400

    added new project amazon clone  in data component

--- src/Component/data/data.js
+++ src/Component/data/data.js
@@ -2,10 +2,25 @@ import image from '../../images/img.png';
 import image1 from '../../images/mympy.png';
 import image2 from '../../images/wanderlust.png';
 import image3 from '../../images/nci.png';
+import image4 from "../../images/amazonclone.png"
 
  export default [
+
     {
         "id":1,
+        "img":image4,
+        "title": "Amazon_Clone",
+        "contain":"Amazon_Clone is a e-commerce web app, where people can do shopping through online.",
+        "technology": "React, HTML, CSS, React Context API, Firebase, Material UI",
+        "githublink":"https://github.com/Manjukcthapa/Amazon-clone",
+        "livelink":"https://manju-amazon.vercel.app/",
+        "responsibility": [
+            "Build the login and signup authentication using firebase",
+            "Created diffrent components like home , product ,checkout , checkout product, subtotal, and proccedcheckout pages using React Context API and diffrent styling libaries "
+        ]
+    },
+    {
+        "id":2,
         "img":image,
         "title": "Picasso Web App",
         "contain":"Picasso is a web app, where user can upload their pic and it coverts to Picasso painting.",
@@ -19,7 +34,7 @@ import image3 from '../../images/nci.png';
     },
 
     {
-        "id":2,
+        "id":3,
         "img":image1,
         "title":"Mympy Dreamers",
         "contain":"Mympy Dream is a marketplace where individuals can create a profile and post their project and needs and find funding from the Mymphy Dreams community.",
@@ -32,7 +47,7 @@ import image3 from '../../images/nci.png';
         ]
     },
     {
-        "id":3,
+        "id":4,
         "img":image2,
         "title":"Wanderlust",
         "contain":"Single Project application for tourist lovers. A place for guides and tourists to meet and share their experience." ,
@@ -45,7 +60,7 @@ import image3 from '../../images/nci.png';
         ]
     },
     {
-        "id":4,
+        "id":5,
         "img":image3,
         "title":"Intl Child Nutrition",
         "contain":"International Child Nutrition is a web application. The purpose of creating this application was to keep tracking the record of child screening. Working as a Backend with 5 developers.",

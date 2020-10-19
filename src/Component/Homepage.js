d356573ed7a5d4dcfbcc35fb42b3b1950a57eb31
Manju KC <kcmanjuthapa@gmail.com>
2020-10-19 15:17:45 -0400

    fixed the img and context of about page

--- src/Component/Homepage.js
+++ src/Component/Homepage.js
@@ -8,41 +8,43 @@ class HomePage extends React.Component {
       <div className="homepage">
         <img className="team-img1" alt="profile" src={image} />
         <div className="homepage-context">
-        <h1>Manju KC</h1>
-        <p className="aboutme">
-          I am a Full Stack Developer with a passion for solving problems. I
-          recently completed Lambda School, a 9-month coding academy. During my
-          free time, I love to go hiking, camping, fishing as well as love to
-          attend local meetups. I am available for fulltime employment and can
-          start immediately. I am currently located in Atlanta, GA but willing
-          to relocate.
-        </p>
-        <h1>Skills</h1>
-        <p className="aboutme">
-          HTML | CSS | SASS | JavaScript | React | React Hooks | Redux |
-          Responsive Design | Styled Component| Node JS | Python | Sqlite3 |
-          Postgres | Express | Knex | Jest | Heroku | Netlify
-        </p>
+          <h1>Manju KC</h1>
+          <p className="about-me">
+            I am a Full Stack Developer with a passion for solving problems. I
+            recently completed Lambda School, a 9-month coding academy. During
+            my free time, I love to go hiking, camping, fishing as well as love
+            to attend local meetups. I am available for fulltime employment and
+            can start immediately. I am currently located in Atlanta, GA but
+            willing to relocate.
+          </p>
+          <h1>Skills</h1>
+          <p className="aboutme">
+            HTML | CSS | SASS | JavaScript | React | React Hooks | Redux |
+            Responsive Design | Styled Component| Node JS | Python | Sqlite3 |
+            Postgres | Express | Knex | Jest | Heroku | Netlify
+          </p>
 
-        <h2 className="icon-text">Find me on...</h2>
-        <li>
-          <a
-            target="_blank"
-            rel="noopener noreferrer"
-            href="https://github.com/Manjukcthapa"
-          >
-            <i className="fab fa-github fa-2x" />
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            rel="noopener noreferrer"
-            href="https://www.linkedin.com/in/manjukc/"
-          >
-            <i className="fab fa-linkedin fa-2x" />
-          </a>
-        </li>
+          <h2 className="icon-text">Find me on...</h2>
+          <div className="home-icon">
+            <li className="home-item">
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://github.com/Manjukcthapa"
+              >
+                <i className="fab fa-github fa-2x" />
+              </a>
+            </li>
+            <li className="home-item">
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://www.linkedin.com/in/manjukc/"
+              >
+                <i className="fab fa-linkedin fa-2x" />
+              </a>
+            </li>
+          </div>
         </div>
       </div>
     );
